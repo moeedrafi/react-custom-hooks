21c924edade101231425d0d59be2c51d08a172dd
Moeed Rafi <moeedrafi95@gmail.com>
2025-05-13 20:06:52 +0500

    feat: useDocumentTitle hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react";
+import { useDocumentTitle } from "./hooks/useDocumentTitle";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  useDocumentTitle(`Clicked ${count} times`);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <section>
+      <h1>useDocumentTitle</h1>
+      <h6>
+        <a
+          className="link"
+          href="https://6vmc1n.csb.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Try in a new tab
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </h6>
+      <button className="primary" onClick={() => setCount(count + 1)}>
+        Increment Count: {count}
+      </button>
+    </section>
+  );
 }
-
-export default App
